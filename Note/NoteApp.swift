7805b0824cdff12a2148e7cc2d3cda974d77d85e
Pond <6410685033@student.tu.ac.th>
2024-04-03 23:48:01 +0700

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Thammasat Thonggamgaew on 3/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
